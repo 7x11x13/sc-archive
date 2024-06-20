e8bf844f7f1fbaeef11beed93615377833e9a788
7x11x13 <x7x11x13@gmail.com>
2024-06-20 16:15:58 -0400

    Update scdl version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="sc-archive",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     author="7x11x13",
     install_requires=[
         "pika",
         "psycopg2",
         "requests",
-        "scdl>=2.7.12",
+        "scdl>=2.8.0",
         "soundcloud-v2>=1.3.7",
         "sqlalchemy>=1.4.0,<2.0.0",
     ],
