21edaf15117873395325f37be010e55c4fa9a3ed
7x11x13 <x7x11x13@gmail.com>
2024-06-24 15:27:39 -0400

    Use discord-webhook library

--- sc_archive/watcher_webhook.py
+++ sc_archive/watcher_webhook.py
@@ -1,75 +1,71 @@
 import datetime
 import json
 import logging
+import pathlib
 
 import pika
-import requests
+from discord_webhook import DiscordEmbed, DiscordWebhook
 
 from .config import init_config
 from .rabbit import init_rabbitmq
 
 config = init_config()
 
-def make_error_webhook_data(error: str) -> dict:
-    return {
-        "username": "SoundCloud",
-        "embeds": [
-            {
-                "type": "rich",
-                "description": error,
-                "color": 0xDC143C,
-                "author": {
-                    "name": "Error",
-                }
-            }
-        ]
-    }
+MAX_DISCORD_FILE_SIZE = 25 * 1024 * 1024  # 25 MiB
 
-def make_artist_webhook_data(artist: dict) -> dict:
-    return {
-        "username": "SoundCloud",
-        "embeds": [
-            {
-                "type": "rich",
-                "timestamp": datetime.datetime.fromtimestamp(artist["last_modified"]).isoformat() + "Z",
-                "author": {
-                    "name": artist["username"],
-                    "url": artist["permalink_url"],
-                    "icon_url": artist["avatar_url"]
-                }
-            }
-        ]
-    }
 
-def make_track_webhook_data(track: dict, artist: dict) -> dict:
-    return {
-        "username": "SoundCloud",
-        "embeds": [
-            {
-                "type": "rich",
-                "title": track["title"],
-                "description": track["description"],
-                "url": track["permalink_url"],
-                "timestamp": datetime.datetime.fromtimestamp(track["last_modified"]).isoformat() + "Z",
-                "thumbnail": {
-                    "url": track["artwork_url"]
-                },
-                "author": {
-                    "name": artist["username"],
-                    "url": artist["permalink_url"],
-                    "icon_url": artist["avatar_url"]
-                }
-            }
-        ]
-    }
+def make_error_webhook_data(error: str) -> DiscordWebhook:
+    webhook = DiscordWebhook("")
+    webhook.username = "SoundCloud"
+    webhook.add_embed(
+        DiscordEmbed(description=error, color=0xDC143C, author={"name": "Error"})
+    )
+    return webhook
+
+
+def make_artist_webhook_data(artist: dict) -> DiscordWebhook:
+    webhook = DiscordWebhook("")
+    webhook.username = "SoundCloud"
+    webhook.add_embed(
+        DiscordEmbed(
+            timestamp=artist["last_modified"],
+            author={
+                "name": artist["username"],
+                "url": artist["permalink_url"],
+                "icon_url": artist["avatar_url"],
+            },
+        )
+    )
+    return webhook
+
+
+def make_track_webhook_data(track: dict, artist: dict) -> DiscordWebhook:
+    webhook = DiscordWebhook("")
+    webhook.username = "SoundCloud"
+    webhook.add_embed(
+        DiscordEmbed(
+            title=track["title"],
+            description=track["description"],
+            url=track["permalink_url"],
+            timestamp=track["last_modified"],
+            thumbnail={"url": track["artwork_url"]},
+            author={
+                "name": artist["username"],
+                "url": artist["permalink_url"],
+                "icon_url": artist["avatar_url"],
+            },
+        )
+    )
+    return webhook
+
 
 def artist_callback(ch: pika.channel.Channel, method, properties, body):
     try:
         data = json.loads(body.decode("utf-8"))
-        webhook_data = make_artist_webhook_data(data["artist"])
+        webhook = make_artist_webhook_data(data["artist"])
         if data["event"] == "updated":
-            webhook_data["embeds"][0]["color"] = 0xffbf1c
-            webhook_url = config.get("watcher_webhook", "artist_updated_webhook")
+            webhook.embeds[0]["color"] = 0xFFBF1C
+            webhook.url = config.get("watcher_webhook", "artist_updated_webhook")
             content = "```\n"
             for attr, (old_value, new_value) in data["changes"].items():
                 old_value = str(old_value)
@@ -78,20 +74,20 @@ def artist_callback(ch: pika.channel.Channel, method, properties, body):
                 new_value = new_value.replace("`", "\\`")
                 content += f"{attr}: {old_value} -> {new_value}\n"
                 if attr == "avatar_url":
-                    webhook_data["embeds"].append(
-                        {"title": "Old", "image": {"url": old_value}}
+                    webhook.add_embed(
+                        DiscordEmbed(title="Old", image={"url": old_value})
                     )
-                    webhook_data["embeds"].append(
-                        {"title": "New", "image": {"url": new_value}}
+                    webhook.add_embed(
+                        DiscordEmbed(title="New", image={"url": new_value})
                     )
             content += "```"
-            webhook_data["content"] = content
+            webhook.content = content
         elif data["event"] == "deleted":
-            webhook_data["embeds"][0]["color"] = 0xDC143C
-            webhook_url = config.get("watcher_webhook", "artist_deleted_webhook")
+            webhook.embeds[0]["color"] = 0xDC143C
+            webhook.url = config.get("watcher_webhook", "artist_deleted_webhook")
         else:
             return
-        result = requests.post(webhook_url, json=webhook_data)
+        result = webhook.execute()
         result.raise_for_status()
     except:
         logging.exception("Could not send data")
@@ -99,11 +95,11 @@ def artist_callback(ch: pika.channel.Channel, method, properties, body):
 def track_callback(ch: pika.channel.Channel, method, properties, body):
     try:
         data = json.loads(body.decode("utf-8"))
-        webhook_data = make_track_webhook_data(data["track"], data["artist"])
+        webhook = make_track_webhook_data(data["track"], data["artist"])
         content = f"Path: `{data['track']['file_path']}`"
         if data["event"] == "updated":
-            webhook_data["embeds"][0]["color"] = 0xffbf1c
-            webhook_url = config.get("watcher_webhook", "track_updated_webhook")
+            webhook.embeds[0]["color"] = 0xFFBF1C
+            webhook.url = config.get("watcher_webhook", "track_updated_webhook")
             content = "\n```\n"
             for attr, (old_value, new_value) in data["changes"].items():
                 old_value = str(old_value)
@@ -112,24 +108,29 @@ def track_callback(ch: pika.channel.Channel, method, properties, body):
                 new_value = new_value.replace("`", "\\`")
                 content += f"{attr}: {old_value} -> {new_value}\n"
                 if attr == "artwork_url":
-                    webhook_data["embeds"].append(
-                        {"title": "Old", "image": {"url": old_value}}
+                    webhook.add_embed(
+                        DiscordEmbed(title="Old", image={"url": old_value})
                     )
-                    webhook_data["embeds"].append(
-                        {"title": "New", "image": {"url": new_value}}
+                    webhook.add_embed(
+                        DiscordEmbed(title="New", image={"url": new_value})
                     )
             content += "```"
         elif data["event"] == "created":
-            webhook_data["embeds"][0]["color"] = 0x8eff1c
-            webhook_url = config.get("watcher_webhook", "track_created_webhook")
+            webhook.embeds[0]["color"] = 0x8EFF1C
+            webhook.url = config.get("watcher_webhook", "track_created_webhook")
         elif data["event"] == "deleted":
-            webhook_data["embeds"][0]["color"] = 0xDC143C
-            webhook_url = config.get("watcher_webhook", "track_deleted_webhook")
+            webhook.embeds[0]["color"] = 0xDC143C
+            base_path = config.get("system", "data_path")
+            path = pathlib.Path(base_path, data["track"]["file_path"])
+            if path.stat().st_size < MAX_DISCORD_FILE_SIZE:
+                with open(path, "rb") as f:
+                    webhook.add_file(f.read(), path.name)
+            webhook.url = config.get("watcher_webhook", "track_deleted_webhook")
         else:
             return
 
-        webhook_data["content"] = content
-        result = requests.post(webhook_url, json=webhook_data)
+        webhook.content = content
+        result = webhook.execute()
         result.raise_for_status()
     except:
         logging.exception("Could not send data")
@@ -137,9 +138,9 @@ def track_callback(ch: pika.channel.Channel, method, properties, body):
 def error_callback(ch: pika.channel.Channel, method, properties, body):
     try:
         msg = body.decode("utf-8")
-        webhook_data = make_error_webhook_data(msg)
-        webhook_url = config.get("watcher_webhook", "error_webhook")
-        result = requests.post(webhook_url, json=webhook_data)
+        webhook = make_error_webhook_data(msg)
+        webhook.url = config.get("watcher_webhook", "error_webhook")
+        result = webhook.execute()
         result.raise_for_status()
     except:
         logging.exception("Could not send data")
--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     packages=find_packages(),
     author="7x11x13",
     install_requires=[
+        "discord-webhook",
         "pika",
         "psycopg2",
-        "requests",
         "scdl>=2.9.5",
         "soundcloud-v2>=1.3.7",
         "sqlalchemy>=1.4.0,<2.0.0",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     entry_points={"console_scripts": ["sc-archive-run = sc_archive.archive:run"]},
 )
