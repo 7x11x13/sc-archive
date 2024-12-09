7bd84df2a6f450d7a4648b69ac7dcb845f0d34d0
7x11x13 <x7x11x13@gmail.com>
2024-12-09 15:22:36 -0500

    Update scdl version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="sc-archive",
-    version="1.0.3",
+    version="1.0.4",
     packages=find_packages(),
     author="7x11x13",
     install_requires=[
         "discord-webhook",
         "pika",
         "psycopg2",
-        "scdl>=2.11.1",
+        "scdl>=2.12.3",
         "soundcloud-v2>=1.5.3",
         "sqlalchemy>=1.4.0,<2.0.0",
     ],
